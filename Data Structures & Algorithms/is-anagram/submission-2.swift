b3831b94e3827923da1fd38a21df8d89749eef60
Pavan Kumar N <pavankumar.n.46@gmail.com>
2026-04-10 08:15:41 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.swift
@@ -0,0 +1,31 @@
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        var countDict = [Character:Int]()
+
+
+        for char in s.count > t.count ? s : t {
+            if let storedCount = countDict[char] {
+                countDict[char] = storedCount + 1
+            } else {
+                countDict[char] = 1
+            }
+        }
+
+        print("first \(countDict)")
+
+        for char in s.count > t.count ? t : s {
+            if let storedCount = countDict[char] {
+                let subtractedValue = storedCount - 1
+                if subtractedValue == 0 {
+                    countDict[char] = nil
+                } else {
+                    countDict[char] = subtractedValue
+                }
+            } 
+        }
+
+        print(countDict)
+
+        return countDict.isEmpty
+    }
+}
