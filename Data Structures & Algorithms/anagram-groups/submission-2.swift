dbbcbb103df90705f567a418e58ce83ae9f91cca
Pavan Kumar N <pavankumar.n.46@gmail.com>
2026-04-12 15:54:23 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        // O(m * n * 26) = O(m * n)
+        var map: [[Int]: [String]] = [:]
+        for str in strs {
+            var count = Array(repeating: 0, count: 26)
+            for char in str {
+                let index = Int(char.asciiValue! - Character("a").asciiValue!)
+                count[index] += 1
+            }
+            map[count, default:[]].append(str)
+        }
+        return Array(map.values)
+    }
+}
